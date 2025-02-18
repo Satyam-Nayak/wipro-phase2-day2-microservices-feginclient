3add63dc4c1ddfce39253dda02e983a0f410ad74
Satyam-Nayak <satyamn120@gmail.com>
2025-02-18 21:47:58 +0530

    Employee-service added to the git

---
+++ Employee-service/src/main/java/com/wipro/Employee_service/EmployeeServiceApplication.java
@@ -0,0 +1,14 @@
+package com.wipro.Employee_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableFeignClients(basePackages = "com.wipro.Employee_service.feign") // Enable Feign clients
+public class EmployeeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeServiceApplication.class, args);
+	}
+}
