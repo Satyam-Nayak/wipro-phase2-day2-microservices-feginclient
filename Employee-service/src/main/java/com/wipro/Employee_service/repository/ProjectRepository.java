437e06d291baf204f1a3bdfe06221366adecd45c
Satyam-Nayak <satyamn120@gmail.com>
2025-02-18 23:36:28 +0530

    employee file delete due to some code chnages

--- Employee-service/src/main/java/com/wipro/Employee_service/repository/ProjectRepository.java
+++
@@ -1,9 +0,0 @@
-package com.wipro.Employee_service.repository;
-
-import com.wipro.Employee_service.entity.Project;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
-
-public interface ProjectRepository extends JpaRepository<Project, Long> {
-    Optional<Project> findByPcode(Long pcode);
-}
